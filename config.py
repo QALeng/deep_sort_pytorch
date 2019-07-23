9b07692a3a18e10851c9a3f3eb54138eadfe5417
QALeng <2996095050@qq.com>
2019-07-23 17:06:35 +0800

    检测接口写好了，可以实现一个帧一帧的检测

--- config.py
+++ config.py
@@ -1,30 +1,9 @@
-# # #win10
-# my_config={
-#     #demo_yolo3_deepsort.py    cv2
-#     'cv2_flag':True,
-#     #YOLO3/detector.py
-#     'use_cuda':False,
-#     #deep/feature_extractor.py
-#     'map_location_flag':True,
-#     #YOLO3/yolo_utils.py
-#     # 'use_cuda':False,
-#     #main.py   摄像头的数量
-#     'tracker_number':1,
-#     #违规时间   单位s
-#     'bad_time':120,
-#     #判断是否离开时长  单位s
-#     'left_time':1,
-#     #我们需要检测的类型   对应coco.name
-#     #=bicycle car  person
-#     'need':[1, 2,0],
-# }
-
-#Centos7
+# #win10
 my_config={
     #demo_yolo3_deepsort.py    cv2
-    'cv2_flag':False,
+    'cv2_flag':True,
     #YOLO3/detector.py
-    'use_cuda':True,
+    'use_cuda':False,
     #deep/feature_extractor.py
     'map_location_flag':True,
     #YOLO3/yolo_utils.py
@@ -32,10 +11,31 @@ my_config={
     #main.py   摄像头的数量
     'tracker_number':1,
     #违规时间   单位s
-    'bad_time':0.0000000001,
+    'bad_time':0.0001,
     #判断是否离开时长  单位s
-    'left_time':0.0000000001,
+    'left_time':0.0000001,
     #我们需要检测的类型   对应coco.name
-    #person bicycle
-    'need':[0,1, 2],
+    #=bicycle car  person
+    'need':[1, 2,0],
 }
+
+#Centos7
+# my_config={
+#     #demo_yolo3_deepsort.py    cv2
+#     'cv2_flag':False,
+#     #YOLO3/detector.py
+#     'use_cuda':True,
+#     #deep/feature_extractor.py
+#     'map_location_flag':True,
+#     #YOLO3/yolo_utils.py
+#     # 'use_cuda':False,
+#     #main.py   摄像头的数量
+#     'tracker_number':1,
+#     #违规时间   单位s
+#     'bad_time':0.0000000001,
+#     #判断是否离开时长  单位s
+#     'left_time':0.0000000001,
+#     #我们需要检测的类型   对应coco.name
+#     #person bicycle
+#     'need':[0,1, 2],
+# }
--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 from demo_yolo3_deepsort import Detector
+import  time
+import  cv2
 
-
-detector=Detector()
-detector.detect()
+width=640
+height=360
+detector=Detector(width,height)
+video_path='./video/2.mp4'
+cv2.namedWindow("test", cv2.WINDOW_NORMAL)
+cv2.resizeWindow("test", 800, 600)
+vdo=cv2.VideoCapture()
+stream=vdo.open(video_path)
+while vdo.grab():
+    start_time = time.time()
+    print('now_time: ',start_time)
+    _, ori_im = vdo.retrieve()
+    result1,result2=detector.detect(ori_im,start_time)
+    # print(result1)
+    cv2.imshow("test", result1)
+    cv2.waitKey(1)
