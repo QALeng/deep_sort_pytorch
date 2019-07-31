f6aa9854de7197b548442b2ec1340a359f3bc1e7
QALeng <2996095050@qq.com>
2019-07-31 13:24:26 +0800

    第三次清理测试文件，之前清理文件之后未测试。这次测试可用

--- main.py
+++ main.py
@@ -5,63 +5,65 @@ import  time
 import  cv2
 import json
 
+
 #读取配置文件函数
 def read_config(configPath,system):
     with open(configPath, 'r', encoding='utf-8') as f:
         all = f.read()
         temp = json.loads(all)[system]
-        if temp['use_cuda'] == 'True':
-            temp['use_cuda'] = True
-        else:
-            temp['use_cuda'] = False
-        if temp['map_location_flag']=='True':
-            temp['map_location_flag']=True
-        else:
-            temp['map_location_flag']=False
-        if temp['cv2_flag'] == 'True':
-            temp['cv2_flag'] = True
-        else:
-            temp['cv2_flag'] = False
+        translate_dict={'True':True,"False":False}
+        keys=['use_cuda','map_location_flag','cv2_flag']
+        for i in keys:
+            temp[i]=translate_dict[temp[i]]
     return temp
+
 
 #配置文件
 configPath='config.json'
 my_config=read_config(configPath,'centos7')
+#视频名称
+video_name='14.mp4'
 #帧的宽，高度
-width,height=1280,720
+width,height=1920,1080
+#自行设置
+fps=24
 #检测函数初始化
 detector=Detector(width,height,my_config)
-video_name='11.mp4'
-video_path='./video/'+video_name
+
+
+#输入视频
+video_dir='./video/'
 vdo=cv2.VideoCapture()
-stream=cv2.VideoCapture(video_path)
-
-
+vdo.open(video_dir+video_name)
 
 
 #pipe初始化
-# rtmp_url = 'rtmp://www.iocollege.com:1935/live/mytest'
-rtm_url='rtmp://video.510link.com:1935/live/streama'
-wp,hp=0.5,0.5
+# rtmp_url='rtmp://video.510link.com:1935/live/streama'
+rtmp_url = 'rtmp://www.iocollege.com:1935/live/mytest'
+#缩放比例
+wp,hp=1,1
 size=(int(width*wp),int(height*hp))
 size_str=str(size[0])+'x'+str(size[1])
-fps=24
 bit='512K'
-pipe=pipe_init(rtm_url,size_str , fps,bit)
+pipe=pipe_init(rtmp_url,size_str , fps,bit)
 
 
 #输出文件
-# 用来设置需要保存视频的格式
+#输入文件的目录
+video_save_dir='./video_pre/'
+# 用来设置需要保存视频的编码方式
 fourcc = cv2.VideoWriter_fourcc(*'MJPG')
-output = cv2.VideoWriter("./video_pre/"+video_name, fourcc, fps, (width, height))
-
+save_name=video_name.split('.')[0]+'.avi'
+output = cv2.VideoWriter(video_save_dir+save_name, fourcc, fps, (width, height))
 
 
 while vdo.grab():
     start_time = time.time()
     _, ori_im = vdo.retrieve()
     ori_im,return_time=detector.detect(1,ori_im,start_time)
-    pipe.stdin.write(ori_im.tostring())  # 存入管道用于直播
     output.write(ori_im)
+    dispose_img=cv2.resize(ori_im,size)
+    pipe.stdin.write(dispose_img.tostring())  # 存入管道用于直播
 
-stream.release()
+
+vdo.release()
